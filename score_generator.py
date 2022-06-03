30cf9bf5f97d7dec4d854ba0ff6f09b34c5d37fc
stephaniiema <54160019+stephaniiema@users.noreply.github.com>
2022-06-03 16:37:04 -0500

    Initial commit

---
+++ score_generator.py
@@ -0,0 +1,65 @@
+import os
+import random
+rhythm_cells=[]
+pitch_sets=[]
+NUM_CELLS=[5,6,8,11,11,8,6,5]
+
+# read in files
+with open("rhythm_cells.txt", 'r') as f:
+    text = f.read()
+    rhythm_cells = text.split('*')
+
+for i in range(len(rhythm_cells)):
+    text = rhythm_cells[i]
+    rhythm_cells[i] = text.split('\n')
+    rhythm_cells[i]=[x for x in rhythm_cells[i] if x]
+
+with open("pitch_sets_2.txt", 'r') as f:
+    text = f.read()
+    pitch_sets = text.split('\n')
+
+for i in range(len(pitch_sets)):
+    text = pitch_sets[i]
+    pitch_sets[i] = text.split(' ')
+    pitch_sets[i]=[x for x in pitch_sets[i] if x]
+
+output=[]
+cell=""
+
+for i in range(len(NUM_CELLS)):
+    output.append([])
+    for j in range(NUM_CELLS[i]):
+        output[i].append([])
+        rand = random.randint(0,len(rhythm_cells[i])-1)
+        cell=str(rhythm_cells[i][rand])
+        cell=cell.split(' ')
+        for x in cell:
+            if x[0]=='b':
+                note = pitch_sets[i][random.randint(0,len(pitch_sets[i])-1)]
+                if not random.randint(0,3):
+                    note = note + '\''
+                output[i][j].append(x.replace('b',note))
+            else:
+                output[i][j].append(x)
+        if j == 4:
+            output[i][j].append('\\bar \"||\" \\break\n')
+
+
+for i in range(len(output)):
+    for j in range(len(output[i])):
+     output[i][j]= ' '.join(output[i][j])
+    output[i]='\n'.join(output[i])
+output = ' '.join(output)
+
+
+ly_file = ''
+with open("template.ly", 'r') as f:
+    ly_file = f.read()
+
+ly_file =ly_file.replace('*', output)
+
+with open("score.ly", 'w') as f:
+    n = f.write(ly_file)
+
+os.system('lilypond score.ly')
+
---
+++ template.ly
@@ -0,0 +1,13 @@
+melody = \fixed c' {
+  \clef treble
+  \key c \major
+  \time 2/4
+
+  *
+}
+
+\score {
+  \new Staff \melody
+  \layout { }
+  \midi { }
+}
